e363e365b6ec46e53154d3f76c5436bec3db19ed
陶泽宇 <hztaozeyu@corp.netease.com>
2016-12-27 10:06:23 +0800

    注释r3_define.h的代码

--- r3/project-mac/R3/R3/Sources/include/r3_define.h
+++ r3/project-mac/R3/R3/Sources/include/r3_define.h
@@ -19,19 +19,19 @@ typedef unsigned char bool;
 #    define TRUE 1
 #endif
 
-#define SHOWINFO 0
-#ifdef SHOWINFO 
-
-#define info(fmt, ...) \
-            do { fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
-
-#define debug(fmt, ...) \
-        do { fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
-                                __LINE__, __func__, __VA_ARGS__); } while (0)
-
-#else
+//#define SHOWINFO 0
+//#ifdef SHOWINFO 
+//
+//#define info(fmt, ...) \
+//            do { fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
+//
+//#define debug(fmt, ...) \
+//        do { fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+//                                __LINE__, __func__, __VA_ARGS__); } while (0)
+//
+//#else
 #define info(...);
 #define debug(...);
-#endif
+//#endif
 
 #endif /* !DEFINE_H */
